b6b9d92cf6d2dfeca0744473fa94497af3e837e7
Your Name <janeyatestheartist@gmail.com>
2025-01-23 10:52:39 +0000

    Updated vanilla-basic with static web app content

--- src/api/hello.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (context, req) {
-    context.res = {
-        // HTTP status code
-        status: 200,
-        // Response body
-        body: { message: "Hello, this is your API!" },
-    };
-};
